219969de71426c218e5a662fa0ae1407dca6f230
Abenezer Addisu <abenezeraddisu24@gmail.com>
2023-06-15 15:12:32 +0300

    operate file updated

--- src/logic/operate.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from 'react-test-renderer';
-import Quotes from '../components/Quotes';
-
-it('Qoute page is working', () => {
-  const tree = renderer.create(<Quotes />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
