153519d2470d45bfdb3f07d77f76c538a0680d12
abenezer-addisu <dasesatube@gmail.com>
2023-06-16 09:18:31 +0300

    add Jest snapshots and testing updated

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Calculator from '../components/Calculator';
+import calculate from './calculate';
 
-it('Rendering 3', () => {
-  const tree = renderer.create(<Calculator />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('calculate', () => {
+  it('set the screen to 0', () => {
+    const result = calculate({ total: null, next: null, operation: null }, 'AC');
+    expect(result).toEqual({ total: null, next: null, operation: null });
+  });
+
+  it('add 2 numbers', () => {
+    const result = calculate({ total: '1', next: '2', operation: '+' }, '=');
+    expect(result).toEqual({ total: '3', next: null, operation: null });
+  });
+
+  it('subtract 2 numbers', () => {
+    const result = calculate({ total: '1', next: '2', operation: '-' }, '=');
+    expect(result).toEqual({ total: '-1', next: null, operation: null });
+  });
+
+  it('multiply 2 numbers', () => {
+    const result = calculate({ total: '1', next: '2', operation: 'x' }, '=');
+    expect(result).toEqual({ total: '2', next: null, operation: null });
+  });
+
+  it('divide 2 numbers', () => {
+    const result = calculate({ total: '1', next: '2', operation: '÷' }, '=');
+    expect(result).toEqual({ total: '0.5', next: null, operation: null });
+  });
 });
