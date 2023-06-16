fa3327c71e31174479638ffeba800c5bceb5eec9
abenezer-addisu <dasesatube@gmail.com>
2023-06-16 09:52:23 +0300

    operate testing updated

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import operate from './operate';
 
 it('adds two numbers', () => {
@@ -19,12 +18,4 @@ it('divides two numbers', () => {
 
 it('modulos two numbers', () => {
   expect(operate(1, 2, '%')).toBe('1');
-=======
-import renderer from 'react-test-renderer';
-import Quotes from '../components/Quotes';
-
-it('Qoute page is working', () => {
-  const tree = renderer.create(<Quotes />).toJSON();
-  expect(tree).toMatchSnapshot();
->>>>>>> 63de86e52a105fa2080730c2bba8c83dd72c2020
 });
